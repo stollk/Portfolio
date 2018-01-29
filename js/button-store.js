9bbeee5af95aba92a51c5e6c1593b78cf6a69ac3
stollk <stolleykaitlyn@gmail.com>
2018-01-29 14:50:25 -0500

    Updated Image links and button-store function

--- js/button-store.js
+++ js/button-store.js
@@ -1,6 +1,6 @@
 $(function() {
   $(".portfolio_btn").click(function(){
-    var get_number = $(this).attr('id');
+    var get_number = $(this).attr('data-number');
     console.log(get_number);
     localStorage.setItem("btn_number", get_number);
     var number = localStorage.getItem("btn_number");
